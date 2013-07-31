1766049141564a67bf4f4ed115a49b2c23d7f40f
Miguel Yanez <miguel.themissinglink@gmail.com>
2013-07-30 23:03:44 -0700

    basic tester script. will flush out further soon

---
+++ tester.py
@@ -0,0 +1,37 @@
+
+import time
+import datetime
+
+from scraper_utils import *
+from scraper_controller import run_all
+
+from datetime import *
+from mongoengine import *
+
+if __name__ == '__main__':
+	
+	connect('flight_scraper')
+	origin = "SEA"
+	dest = "NYC"
+
+	TODAY = date.today()
+	weekdays = (FR,SU)
+	until_date = datetime.strptime('12-01-2013', '%m-%d-%Y')
+	date_pairs = generate_date_pairs(DAILY, weekdays, TODAY, until_date)
+	
+	#Polls search engines
+	#search_flights(date_pairs, origin, dest)51
+
+
+	#test 2 - eventually this will be graphed and displayed in webpage, for now CSV
+	# X axis: query_dates
+	# Y axis: prices
+	output = open("results.csv", "w")
+	date_pair = [datetime.strptime('08-16-2013', '%m-%d-%Y'), datetime.strptime('08-18-2013', '%m-%d-%Y')]
+	
+	result= get_all_prices_for_date_pair(date_pair)
+	for r in result:
+		for p in result[r]:
+			output.write("%s, %s\n" % (r,p))
+
+	output.close()
